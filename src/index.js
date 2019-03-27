0089e4fb682b98f42b349f8fb25f9d32a973fde4
Aaron Pulley <awpulley1@gmail.com>
2019-03-26 23:13:34 -0400

    Date manipulation
    Generating, passing, and printing dates. Also, ignore nbproject folder for git.

--- src/index.js
+++ src/index.js
@@ -2,15 +2,48 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+class DateDisplay extends React.Component {
+    render() {
+        return (
+            <div className="date-display">
+                {this.props.dayName(this.props.currDate)}<br />
+                {this.props.currDate.getDate()}<br />
+                {this.props.monthName(this.props.currDate)} {this.props.currDate.getFullYear()}
+            </div>
+        );
+    }
+};
+
 class App extends React.Component {
+    constructor (props) {
+        super (props);
+        this.state = {
+            currDate: new Date(),
+            dayNames: ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", 
+                    "Friday", "Saturday"],
+            monthNames: ["January", "February", "March", "April", "May", "June", 
+                    "July", "August", "September", "October", "November", "December"]
+        };
+    }
+    
+    dayName(dateObj) {
+        return (this.state.dayNames[dateObj.getDay()]);
+    }
+    
+    monthName(dateObj) {
+        return (this.state.monthNames[dateObj.getMonth()]);
+    }
+    
     render() {
         return (
             <div className="app">
                 <div className="schedule-page">
                     <header className="date-picker">
-                        <div className="date-display">
-                        Display <br />date
-                        </div>
+                        <DateDisplay
+                            currDate={this.state.currDate}
+                            dayName={(dateObj) => this.dayName(dateObj)}
+                            monthName={(dateObj) => this.monthName(dateObj)}
+                        />
                         <div className="date-select">
                             Select date
                         </div>
