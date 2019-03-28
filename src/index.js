eb8c67c9a741249d589b0742396f467797e69761
Aaron Pulley <awpulley1@gmail.com>
2019-03-27 20:30:32 -0400

    Notes in comments

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+//import DateDisplay from ./filehere.txt;
 
 class DateDisplay extends React.Component {
     render() {
